ad8d56a107392080f15dcf1d4f838c7033c745e8
d.kayukov@gmail.com <d.kayukov@gmail.com@5c42c5a8-900f-5b5a-2f8c-d720ba63385b>
2011-09-23 06:48:12 +0000

    Initial commit

--- Plush30A.cmd
+++ Plush30A.cmd
@@ -1,5 +1,5 @@
 @Echo Off
-call _make.cmd Plush30A pwm_rc_200
+call _make.cmd Plush30A pwm_arduino_250
 pause
 avrdude.exe -C "H:\Program Files\arduino-0022\hardware\tools\avr\etc\avrdude.conf" -v -p m8 -P com3  -c avrisp -b 19200 -U flash:w:bin\Plush30A.hex 
 
