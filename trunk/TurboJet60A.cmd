5bdedc7a1796a0c240ec45a68c35e03847b2ec9a
d.kayukov@gmail.com <d.kayukov@gmail.com@5c42c5a8-900f-5b5a-2f8c-d720ba63385b>
2012-08-15 01:00:39 +0000

    The latest 1.1 version is moved from trunk to the \tags\1.1

--- trunk/TurboJet60A.cmd
+++
@@ -1,7 +0,0 @@
-@Echo Off
-call _make.cmd TurboJet60A pwm_rc_200
-pause
-avrdude.exe -C "H:\Program Files\arduino-0022\hardware\tools\avr\etc\avrdude.conf" -v -p m8 -P com3  -c avrisp -b 19200 -U flash:w:bin\TurboJet60A.hex 
-
-
-
