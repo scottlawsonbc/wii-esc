91450d934d8849b533ff4e29fbde90bbdc5c85d7
d.kayukov@gmail.com <d.kayukov@gmail.com@5c42c5a8-900f-5b5a-2f8c-d720ba63385b>
2012-12-07 04:39:29 +0000

    wii-esc-flash: USBLinker boot loader. (fresh version)

--- bootloader/USBLinker/_make.cmd
+++ bootloader/USBLinker/_make.cmd
@@ -4,4 +4,5 @@ SET AS="%ProgramFiles%\AvrAssembler2\avrasm2.exe"
 SET AS_FLAGS= -I "%ProgramFiles%\AvrAssembler2\AppNotes"
 SET OUT=bin
 
+wget https://raw.github.com/sim-/tgy/master/boot.inc
 %AS% %AS_FLAGS%  -S "%OUT%\labels.tmp" -fI -W+ie -o "%OUT%\USBLinker.hex" -d "%OUT%\USBLinker.obj" -m "%OUT%\USBLinker.map" -l "%OUT%\USBLinker.lst" "USBLinker.asm"
